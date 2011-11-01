640b304fd5301e9cd700a1221f31ea164891a283
Jean-Philippe Guérard <jean-philippe.guerard@tigreraye.org>
2011-11-01 13:49:05 +0100

    Remise en forme du code source
    Formatage du code source de la documentation en limitant la
    taille des lignes à 80 caractères.

--- doc.php
+++ doc.php
@@ -16,7 +16,8 @@
 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301  USA.
  */
 
 require("./includes/config.inc.php");
@@ -26,53 +27,77 @@ require("./includes/header.inc.php");
 
 <h2>Introduction</h2>
 
-<p>
-Ce glossaire a été créé dans le but de faciliter et d'uniformiser la traduction de logiciels et de documentation informatique.
-Cette page sert de documentation pour l'utilisation du système.
-</p>
+<p>Ce glossaire a été créé dans le but de faciliter et d'uniformiser
+la traduction de logiciels et de documentation informatique.
+Cette page sert de documentation pour l'utilisation du système.</p>
 
-<p>L'utilisation du glossaire permet d'avoir une traduction homogène. Faites cependant attention à bien prendre en compte le contexte de la traduction et à ne pas appliquer bêtement les traductions indiquées dans ce document.</p>
+<p>L'utilisation du glossaire permet d'avoir une traduction homogène.
+Faites cependant attention à bien prendre en compte le contexte de la
+traduction et à ne pas appliquer bêtement les traductions indiquées
+dans ce document.</p>
 
 <h2>Utilisateurs</h2>
-Les utilisateurs du glossaire sont divisés en trois catégories décrites ci-après.
+
+<p>Les utilisateurs du glossaire sont divisés en trois catégories
+décrites ci-après.</p>
+
 <h3>Visiteurs</h3>
-Un utilisateur qui n'est pas connecté est considéré comme un simple visiteur. 
-Il peut accéder au glossaire en lecture seule ; c'est à dire effectuer des recherches et exporter des données.
-Afin d'accéder à plus de fonctions, il est nécessaire que le visiteur s'enregistre et se connecte au glossaire ou demande son inscription en tant qu'administrateur sur la <a href="http://www.traduc.org/mailman/listinfo/glossaire">liste de diffusion</a>.
+
+<p>Un utilisateur qui n'est pas connecté est considéré comme un simple
+visiteur. Il peut accéder au glossaire en lecture seule&nbsp;;
+c'est-à-dire effectuer des recherches et exporter des données.
+Afin d'accéder à plus de fonctions, il est nécessaire que le visiteur
+s'enregistre et se connecte au glossaire ou demande son inscription
+en tant qu'administrateur sur la
+<a href="http://www.traduc.org/mailman/listinfo/glossaire">liste
+de diffusion</a>.</p>
 
 <h3>Utilisateurs enregistrés</h3>
-Un utilisateur enregistré à les mêmes capacités qu'un visiteur avec en plus la possibilité de maintenir son glossaire personnel. Les éléments ajoutés dans le glossaire personnel sont visibles de tous mais ne font pas partie intégrante du glossaire officiel.
+
+<p>Un utilisateur enregistré à les mêmes capacités qu'un visiteur avec
+en plus la possibilité de maintenir son glossaire personnel. Les éléments
+ajoutés dans le glossaire personnel sont visibles de tous mais ne font
+pas partie intégrante du glossaire officiel.</p>
 
 <h3>Administrateurs</h3>
-Les administrateurs sont désignés par d'autres administrateurs après une demande motivée sur la <a href="http://www.traduc.org/mailman/listinfo/glossaire">liste de diffusion</a>. Ils ont la capacité de modifier les glossaires officiels et personnels et de supprimer ou modifier les utilisateurs enregistrés et les administrateurs.
+
+<p>Les administrateurs sont désignés par d'autres administrateurs après
+une demande motivée sur la
+<a href="http://www.traduc.org/mailman/listinfo/glossaire">liste de
+diffusion</a>. Ils ont la capacité de modifier les glossaires officiels
+et personnels et de supprimer ou modifier les utilisateurs enregistrés
+et les administrateurs.</p>
 
 <h2>Exportation</h2>
-Le glossaire officiel peut être <a href="export.php">exporté</a> dans divers formats dont CSV, XML, etc.
+
+<p>Le glossaire officiel peut être <a href="export.php">exporté</a>
+dans divers formats dont CSV, XML, etc.</p>
 
 <h2>Importation</h2>
-Des glossaires pourront être importés dans le glossaire officiel par les administrateurs. Les formats supportés sont notamment CSV, XML, etc.
+
+<p>Des glossaires pourront être importés dans le glossaire officiel
+par les administrateurs. Les formats supportés sont notamment CSV, XML,
+etc.</p>
 
 <h2>Recherche dans Firefox</h2>
 
-Vous pouvez ajouter le glossaire comme moteur de recherche dans Firefox : <a href="#" onclick="addEngine();">ajouter à Firefox</a>.
-
+<p>Vous pouvez ajouter le glossaire comme moteur de recherche dans
+Firefox&nbsp;: <a href="#" onclick="addEngine();">ajouter à
+Firefox</a>.</p>
 
 <h2>Organisation</h2>
 
-<p>
+<p>Le glossaire inter-projet a été créé en commun par l'association
+<a href="http://www.traduc.org">Traduc.org</a>,
+l'<a href="http://fr.openoffice.org">Équipe francophone
+d'OpenOffice.org</a>, le <a href="http://gnomefr.traduc.org/">Groupe de
+travail de traduction de GNOME en Français</a> et le projet
+<a href="http://fr.l10n.kde.org/">KDE en français</a>.</p>
 
-Le glossaire inter-projet a été créé en commun par l'association <a href="http://www.traduc.org">Traduc.org</a>, l'<a href="http://fr.openoffice.org">Équipe francophone 
-d'OpenOffice.org</a>, le <a href="http://gnomefr.traduc.org/">Groupe de travail de traduction de GNOME en Français</a> et le projet <a href="http://fr.l10n.kde.org/">KDE en français</a>.
-
-</p>
-
-<p>
-
-L'évolution de ce glossaire, ainsi que son contenu sont discutés sur la liste <a 
-href="http://www.traduc.org/mailman/listinfo/glossaire">glossaire CHEZ 
-traduc POINT org</a>.
-
-</p>
+<p>L'évolution de ce glossaire, ainsi que son contenu sont discutés
+sur la liste
+<a href="http://www.traduc.org/mailman/listinfo/glossaire">glossaire CHEZ
+traduc POINT org</a>.</p>
 
 <?php
 require("./includes/footer.inc.php");
