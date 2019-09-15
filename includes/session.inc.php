e5e5cd512b8c05d0af0897b5338366d17fe68b61
Jean-Philippe Guérard <jean-philippe.guerard@tigreraye.org>
2019-09-15 21:16:49 +0200

    Remplacement de Foobar par le nom du projet.

--- includes/session.inc.php
+++ includes/session.inc.php
@@ -17,7 +17,7 @@
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ * along with the Glossary.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 session_start();
